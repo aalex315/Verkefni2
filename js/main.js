f0e81850e25b398d9e49db496e4e48ea27ee1f28
Alexander <allimon315@gmail.com>
2015-09-29 10:58:16 +0000

    Added reset data button

--- js/main.js
+++ js/main.js
@@ -1,25 +1,6 @@
 $(document).ready(function() {
-	$.ajax({
-		'url': 'http://apis.is/concerts',
-		'type': 'GET',
-		'dataType': 'json',
-		'success': function(response){
-			// console.log(response);
+	showAllConcerts()
 
-			// Gets the number of objects
-			var responseLength = Object.keys(response.results).length;
-			// Goes through every object and displays value
-			for(var i = 0; i < responseLength; i++)
-			{
-				var cleanDate = response.results[i].dateOfShow;
-				var cutDate = cleanDate.substring(0,10);
-
-				var data = "<li date='" + response.results[i].dateOfShow + "'><div class='row idfk'><div class='large-8 columns'><span class='title'>" + response.results[i].eventDateName + "</span><br><span class='idk'>" + response.results[i].name + "</span><br><span class='idk'>" + response.results[i].eventHallName + "</span><br><span class='date'>" + cutDate + "</span><span class='group'>" + response.results[i].userGroupName + "</span></div><div class='large-4 columns'><img src='" + response.results[i].imageSource + "'></div></div></li>";
-
-				$(".mainStuff").append(data);
-			}
-		}
-	});
 	// Sets value of datePicker
 	$("#datefrom").datepicker({dateFormat: 'yy-mm-dd'});
 	$("#dateto").datepicker({dateFormat: 'yy-mm-dd'});
@@ -58,4 +39,26 @@ function getConcerts(){
 			}
 		}
 	});
+};
+function showAllConcerts(){
+	$(".mainStuff").empty();
+	$.ajax({
+		'url': 'http://apis.is/concerts',
+		'type': 'GET',
+		'dataType': 'json',
+		'success': function(response){
+			// Gets the number of objects
+			var responseLength = Object.keys(response.results).length;
+			// Goes through every object and displays value
+			for(var i = 0; i < responseLength; i++)
+			{
+				var cleanDate = response.results[i].dateOfShow;
+				var cutDate = cleanDate.substring(0,10);
+
+				var data = "<li date='" + response.results[i].dateOfShow + "'><div class='row idfk'><div class='large-8 columns'><span class='title'>" + response.results[i].eventDateName + "</span><br><span class='idk'>" + response.results[i].name + "</span><br><span class='idk'>" + response.results[i].eventHallName + "</span><br><span class='date'>" + cutDate + "</span><span class='group'>" + response.results[i].userGroupName + "</span></div><div class='large-4 columns'><img src='" + response.results[i].imageSource + "'></div></div></li>";
+
+				$(".mainStuff").append(data);
+			}
+		}
+	})
 }
