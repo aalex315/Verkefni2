93a37b2b680d6768f03eb320408ee501a29e89cf
Alexander <allimon315@gmail.com>
2015-09-30 21:31:42 +0000

    Added time of event

--- js/main.js
+++ js/main.js
@@ -6,42 +6,12 @@ $(document).ready(function() {
 	$("#dateto").datepicker({dateFormat: 'yy-mm-dd'});
 });
 
-function getConcerts(){
-	// Emptys the list
-	$(".mainStuff").empty();
-	// Gets data from input
-	var dateFrom = $("#datefrom").val();
-	var dateTo = $("#dateto").val();
-	$.ajax({
-		'url': 'http://apis.is/concerts',
-		'type': 'GET',
-		'dataType': 'json',
-		'success': function(response){
-			console.log(response);
-			// Gets the number of objects
-			var responseLength = Object.keys(response.results).length;
-
-			for(var i = 0; i < responseLength; i++)
-			{
-				// Puts the date from json into right format
-				var cleanDate = response.results[i].dateOfShow;
-				var cutDate = cleanDate.substring(0,10);
-
-				// checks if object is within date range
-				if (new Date(cutDate) >= new Date(dateFrom) && new Date(cutDate) <= new Date(dateTo)) {
-					var data = "<li date='" + response.results[i].dateOfShow + "'><div class='row idfk'><div class='large-8 columns'><span class='title'>" + response.results[i].eventDateName + "</span><br><span class='idk'>" + response.results[i].name + "</span><br><span class='idk'>" + response.results[i].eventHallName + "</span><br><span class='date'>" + cutDate + "</span><span class='group'>" + response.results[i].userGroupName + "</span></div><div class='large-4 columns'><img src='" + response.results[i].imageSource + "'></div></div></li>";
-				}
-				else if(new Date(cutDate) >= new Date(dateTo)){
-					// End the for loop instead of going through the rest of the objects when it detects the date is more the input date
-					break;
-				}
-				$(".mainStuff").append(data);
-			}
-		}
-	});
-};
 function showAllConcerts(){
 	$(".mainStuff").empty();
+	// Clears the input fields
+	$("#datefrom").val('');
+	$("#dateto").val('');
+
 	$.ajax({
 		'url': 'http://apis.is/concerts',
 		'type': 'GET',
@@ -55,10 +25,56 @@ function showAllConcerts(){
 				var cleanDate = response.results[i].dateOfShow;
 				var cutDate = cleanDate.substring(0,10);
 
-				var data = "<li date='" + response.results[i].dateOfShow + "'><div class='row idfk'><div class='large-8 columns'><span class='title'>" + response.results[i].eventDateName + "</span><br><span class='idk'>" + response.results[i].name + "</span><br><span class='idk'>" + response.results[i].eventHallName + "</span><br><span class='date'>" + cutDate + "</span><span class='group'>" + response.results[i].userGroupName + "</span></div><div class='large-4 columns'><img src='" + response.results[i].imageSource + "'></div></div></li>";
+				var timeOfEvent = cleanDate.substring(12,20);
+
+				var data = "<li date='" + response.results[i].dateOfShow + "'><div class='row idfk'><div class='large-8 columns'><span class='title'>" +
+				 response.results[i].eventDateName + "</span><br><span class='idk'>" +
+				 response.results[i].name + "</span><br><span class='idk'>" +
+				 response.results[i].eventHallName + "</span><br><span class='date'>" +
+				 cutDate + " kl:" + timeOfEvent + "</span><span class='group'>" + response.results[i].userGroupName + 
+				 "</span></div><div class='large-4 columns'><img src='" + response.results[i].imageSource + "'></div></div></li>";
 
 				$(".mainStuff").append(data);
 			}
 		}
 	})
-}
+};
+
+function getConcerts(){
+	// Emptys the list
+	$(".mainStuff").empty();
+	// Gets data from input
+	var dateFrom = $("#datefrom").val();
+	var dateTo = $("#dateto").val();
+	$.ajax({
+		'url': 'http://apis.is/concerts',
+		'type': 'GET',
+		'dataType': 'json',
+		'success': function(response){
+			// Gets the number of objects
+			var responseLength = Object.keys(response.results).length;
+
+			for(var i = 0; i < responseLength; i++)
+			{
+				// Puts the date from json into right format
+				var cleanDate = response.results[i].dateOfShow;
+				var cutDate = cleanDate.substring(0,10);
+
+				// checks if object is within date range
+				if (new Date(cutDate) >= new Date(dateFrom) && new Date(cutDate) <= new Date(dateTo)) {
+					var data = "<li date='" + response.results[i].dateOfShow + "'><div class='row idfk'><div class='large-8 columns'><span class='title'>" +
+					 response.results[i].eventDateName + "</span><br><span class='idk'>" +
+					 response.results[i].name + "</span><br><span class='idk'>" +
+					 response.results[i].eventHallName + "</span><br><span class='date'>" +
+					 cutDate + "</span><span class='group'>" + response.results[i].userGroupName + 
+					 "</span></div><div class='large-4 columns'><img src='" + response.results[i].imageSource + "'></div></div></li>";
+					}
+				else if(new Date(cutDate) >= new Date(dateTo)){
+					// End the for loop instead of going through the rest of the objects when it detects the date is more the input date
+					break;
+				}
+				$(".mainStuff").append(data);
+			}
+		}
+	});
+};
